ad5c1c80a082dcd497a7f8ae11804a1fd66babd4
0xSPR <lb18590244@queretaro.tecnm.mx>
2024-05-14 10:23:11 -0500

    Base Line

---
+++ src/main/java/com/sslife/ssliferest/SsLifeRestApplication.java
@@ -0,0 +1,13 @@
+package com.sslife.ssliferest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsLifeRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsLifeRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sslife/ssliferest/SsLifeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sslife.ssliferest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsLifeRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
